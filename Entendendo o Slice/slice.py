483b0b414652323799c97db63546bfdb2c9a3ace
vinicanalles <62850892+vinicanalles@users.noreply.github.com>
2023-09-04 13:43:16 -0300

    feat: Entendendo o conceito de Slice
    - Aprendendo a trabalhar com os índices de uma variável do tipo string;
    - Cada posição da String possui um índice que podemos usar caso precise recuperar apenas uma parte da String;

---
+++ Entendendo o Slice/slice.py
@@ -0,0 +1,14 @@
+fruta = 'Abacate'
+# index  0123456
+
+print(fruta)  # Exibe o valor inteiro da variável
+print(fruta[2:6])  # Exibe o valor da variável do index 2 ao 6
+print(fruta[-1])  # Exibe o valor da variável do index -1
+
+valor = 99.75
+valor = str(valor)  # Converte o valor para string
+# index 01234
+
+print(valor[3:5])
+print(valor[:2])
+print(valor[2:])
